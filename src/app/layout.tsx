adb62694aa3e27150a677925323d3680ebff3fde
Benjoe Rivera Vidal <benjoe22@gmail.com>
2025-05-10 20:55:33 +0800

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
+
 import Script from 'next/script';
 
 const geistSans = Geist({
@@ -44,6 +46,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
         <Script
           id="adsbygoogle-init"
           strategy="afterInteractive"
