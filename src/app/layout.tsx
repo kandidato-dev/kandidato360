e88e90cf6b41ee74906fb37660b2b793657087fc
Benjoe Rivera Vidal <benjoe22@gmail.com>
2025-05-10 22:08:18 +0800

    Updated monetag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,17 +42,17 @@ export default function RootLayout({
         <meta name="google-adsense-account" content={process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || ''} />
         
       </head>
-     
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Script
+      <Script
           src="https://ligheechoagool.com/88/tag.min.js"
           data-zone="146713"
           async
           data-cfasync="false"
           strategy="afterInteractive"
       />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        
         {children}
         <SpeedInsights />
         <Analytics />
